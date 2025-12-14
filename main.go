e6a4f422b8dd7933b0d8930780f1c6167f211871
ScrufStudios <scrufstudios@gmail.com>
2025-12-14 13:08:53 -0500

    Creating multiplexer and server. Adding gitignore

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	sm := http.NewServeMux()
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: sm,
+	}
+	s.ListenAndServe()
 	fmt.Println("Hello, chirpy")
 }
